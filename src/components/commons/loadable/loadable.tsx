f1041d20900879ce875a057d8625e1b8c426a363
Refilwe Mashile <refilwe@codehesion.co.za>
2023-08-05 18:11:50 +0200

    Import alias

--- src/components/commons/loadable/loadable.tsx
+++ src/components/commons/loadable/loadable.tsx
@@ -1,6 +1,6 @@
 import { FC, Suspense } from "react";
 import { Loader } from "..";
-import { ChildrenProps } from "../../../types";
+import { ChildrenProps } from "@gnerd/types";
 
 export const Loadable: FC<ChildrenProps> = ({ children }) => (
   <Suspense fallback={<Loader />}>{children}</Suspense>
