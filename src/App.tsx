d76defd8594bf4c081f31200f852a0a0b6150cee
Refilwe Mashile <refilwe@codehesion.co.za>
2023-08-05 20:44:23 +0200

    Tailwind linked

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Toaster } from "react-hot-toast";
+
 function App() {
-  return <p className="text-teal-400">Test</p>;
+  return (
+    <>
+      <Toaster position="top-center" />
+      <h1 className="text-teal-400">Test</h1>
+    </>
+  );
 }
 
 export default App;
