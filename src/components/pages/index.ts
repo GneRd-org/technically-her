0e9046688b46d33c745b9d3a9963c0ce8b736a08
Refilwe Mashile <refilwe@codehesion.co.za>
2023-08-05 17:39:39 +0200

    Added pages

---
+++ src/components/pages/index.ts
@@ -0,0 +1,6 @@
+export * from "./culture-fit/culture-fit";
+export * from "./home/home";
+export * from "./interview-simulation/interview-simulation";
+export * from "./landing/landing";
+export * from "./profile/profile";
+export * from "./sign-up/sign-up";
