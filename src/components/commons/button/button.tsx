41c4e6f4d95ca9f24bf8481f3962ee157f31b51e
Refilwe Mashile <refilwe@codehesion.co.za>
2023-08-06 02:27:23 +0200

    Completed Auth

--- src/components/commons/button/button.tsx
+++ src/components/commons/button/button.tsx
@@ -2,21 +2,24 @@ import { FC } from "react";
 import { ChildrenProps } from "../../../types";
 interface ButtonProps extends ChildrenProps {
   buttonType: "stop" | "submit" | "common";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick: (arg0: any) => any;
 }
-export const Button: FC<ButtonProps> = ({ children, buttonType }) => {
-  let style = ""
-    switch (buttonType) {
-        case "submit":
-            style += "bg-[#76FA55]"
-            break
-        case "stop":
-            style += "bg-[#DD5159]"
-            break
-        default:
-            style += "bg-[#90CDF4]"
-    }
+export const Button: FC<ButtonProps> = ({ children, buttonType, ...rest }) => {
+  let style = "";
+  switch (buttonType) {
+    case "submit":
+      style += "bg-[#76FA55]";
+      break;
+    case "stop":
+      style += "bg-[#DD5159]";
+      break;
+    default:
+      style += "bg-[#90CDF4]";
+  }
   return (
-    
-  <button className={style+" rounded-lg py-3 px-3"} >{children}</button>
+    <button {...rest} className={style + " rounded-lg py-3 px-3"}>
+      {children}
+    </button>
   );
 };
