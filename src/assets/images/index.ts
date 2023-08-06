4ba729bb1ebff5330d6e35e34a780986237e9ee6
gumokwe022 <gumokwe022@student.wethinkcode.co.za>
2023-08-06 03:05:45 +0200

    Started on home page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,1 +1,3 @@
 export { default as Learner } from "./learner.png";
+export { default as Interview } from "./Interview.png";
+export { default as culture } from "./culture.png";
--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,36 +1,37 @@
-import { Card } from "../..";
-import { CiUnlock } from "react-icons/ci";
-import { Learner } from "../../../assets";
+import { Button, Footer } from "../..";
+import { Interview } from "../../../assets";
+import { culture } from "../../../assets";
 
 export const Home = () => {
   return (
     <main className="h-screen">
-      <section className="bg-secondaryDark flex items-center">
+      <section className="bg-secondaryDark flex items-center py-3">
         <section>
           <h1 className="text-5xl">
-            Technically<span className="text-teal-400 ">Her</span>
+            Interview Ready
           </h1>
-          <h2 className="text-4xl">Progressive Advancement!</h2>
           <p>
-            Get started on your tech journey with TechnicallyHER, where
-            empowerment meets innovation!
+          Gain the confidence to ace your tech job interviews with our AI-driven interview simulations and personalized coaching. Let's prepare you to stand out and land your dream tech role.
           </p>
+          <Button buttonType={"common"}>Prepare for Interviews</Button>
         </section>
-        <img src={Learner} alt="learner" />
+        <img src={Interview} alt="Interview picture" width={450}/>
       </section>
-      <section className="flex justify-between items-center gap-5 bg-primaryDark">
-        <Card
-          image={<CiUnlock size={40} />}
-          text="Unlock your potential with our comprehensive upskilling courses. Master coding languages and cutting-edge tech trends for the skills the industry demands."
-        />
-        <Card
-          image={<CiUnlock />}
-          text="Unlock your potential with our comprehensive upskilling courses. Master coding languages and cutting-edge tech trends for the skills the industry demands."
-        />
-        <Card
-          image={<CiUnlock />}
-          text="Unlock your potential with our comprehensive upskilling courses. Master coding languages and cutting-edge tech trends for the skills the industry demands."
-        />
+      <section className="flex justify-between items-center gap-5 bg-primaryDark py-3 px-3">
+      <img src={culture} alt="Interview picture" width={450}/>
+        <section>
+          <h1 className="text-5xl">
+          Culture Fit Analysis
+          </h1>
+          <p>
+          We understand the importance of finding the right fit for your career. Our culture fit analysis helps you discover tech companies where you'll thrive and contribute your unique strengths.
+          </p>
+          
+          <Button buttonType={"common"}>Find Your Fit</Button>
+        </section>
+      </section>
+      <section className="bg-secondaryDark justify-center flex items-center text-center">
+        <Footer />
       </section>
     </main>
   );
